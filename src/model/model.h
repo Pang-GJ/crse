70e8f6a12a97b755860d67f95e6213e6764d21d1
Pang-GJ <2362421192@qq.com>
2023-01-01 11:04:24 +0800

    WIP: add BaseModel type define

--- src/model/model.h
+++ src/model/model.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <cstdint>
+#include "common/random.h"
 #include "model/params.h"
 
 // Model is the interface for all models
 class Model {
  public:
   Model() = default;
-  
+
   // Set parameters
   virtual void SetParams(Params params) = 0;
 
@@ -18,4 +20,36 @@ class Model {
 
   // Clear model weights
   virtual void Clear() = 0;
+};
+
+// BaseModel model must be included by every recommendation model.
+// Hyper-parameters, ID sets, random generator and fitting options are managed
+// the BaseModel model.
+class BaseModel : public Model {
+ public:
+  explicit BaseModel(Params params)
+      : params_(std::move(params)),
+        rand_state_(params.GetInt64(RANDOM_STATE, 0)),
+        rng_(rand_state_) {}
+
+  void SetParams(Params params) override {
+    params_ = std::move(params);
+    rand_state_ = params_.GetInt64(RANDOM_STATE, 0);
+    rng_ = RandomGenerator(rand_state_);
+  }
+
+  Params GetParams() override { return params_; }
+
+  ParamsGrid GetParamsGrid() override { return {}; }
+
+  RandomGenerator &GetRandomGenerator() { return rng_; }
+
+  // no thing
+  void Clear() override {}
+
+  Params params_;  // Hyper-parameters
+
+ private:
+  std::int64_t rand_state_;  // random seed
+  RandomGenerator rng_;      // random generator
 };
